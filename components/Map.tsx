37323558836cf2b35596ce1fa40e14578fb7e5ec
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2024-09-03 17:16:21 +0700

    Map Funtionally

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,13 +1,24 @@
 import { View, Text } from "react-native";
 import React from "react";
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
 
 type Props = {};
 
 const Map = (props: Props) => {
+    // const region = {}
     return (
-        <View>
+        <MapView
+            provider={PROVIDER_DEFAULT}
+            className="w-full h-full rounded-2xl"
+            tintColor="black"
+            mapType="mutedStandard"
+            showsPointsOfInterest={false}
+            // initialRegion={region}
+            showsUserLocation={true}
+            userInterfaceStyle="light"
+        >
             <Text>Map</Text>
-        </View>
+        </MapView>
     );
 };
 
